df475a33f53f8e9e658a3550f8be452cf4149095
imtheprince7 <princeks3577@gmail.com>
2024-10-25 20:19:56 +0530

    day-20

--- Arrays_Practice/AlternativeSorting.cpp
+++ Arrays_Practice/AlternativeSorting.cpp
@@ -7,18 +7,18 @@ using namespace std;
 class Solution {
 
   public:
-    vector<int> alternateSort(vector<int>& arr) {
+    vector<int> alternateSort(vector<int>& array) {
         
-        sort(arr.begin(), arr.end());
-        int i = 0, j = arr.size() - 1;
+        sort(array.begin(), array.end());
+        int i = 0, j = array.size() - 1;
         vector<int> answer;
         
         while(i<=j){
             if (i <= j) {
-                answer.push_back(arr[j--]);  // Add the largest remaining element
+                answer.push_back(array[j--]);  // Add the largest remaining element
             }
             if (i <= j) {
-                answer.push_back(arr[i++]);  // Add the smallest remaining element
+                answer.push_back(array[i++]);  // Add the smallest remaining element
             }
         }        
         return answer;
@@ -26,10 +26,9 @@ class Solution {
 };
 
 int main(){
-    vector<int> vect ={7, 1, 2, 3, 4, 5, 6};
+    vector<int> array ={7, 1, 2, 3, 4, 5, 6};
     Solution solution;
-    auto answer = solution.alternateSort(vect);
-    // cout<<answer;
+    auto answer = solution.alternateSort(array);
 
     return 0;
 }
