46cbc3f1fb4b036d025a7e8c4db62fb42f575f3d
imtheprince7 <princeks3577@gmail.com>
2024-11-21 18:05:31 +0530

    nov-21

---
+++ String_Practice/Leetcode_599.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+ public:
+  vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+    vector<string> ans;
+    unordered_map<string, int> restaurantToIndex;
+    int minSum = INT_MAX;
+
+    for (int i = 0; i < list1.size(); ++i)
+      restaurantToIndex[list1[i]] = i;
+
+    for (int i = 0; i < list2.size(); ++i) {
+      const string& restaurant = list2[i];
+      if (const auto it = restaurantToIndex.find(restaurant);
+          it != restaurantToIndex.cend()) {
+        const int sum = it->second + i;
+        if (sum < minSum) {
+          minSum = sum;
+          ans = {restaurant};
+        } else if (sum == minSum) {
+          ans.push_back(restaurant);
+        }
+      }
+    }
+    return ans;
+  }
+
+};
+
+
+int main(){
+
+    vector<string> list1 = {"Shogun","Tapioca Express","Burger King","KFC"};
+    vector<string> list2 = {"Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"};
+    
+    auto answer = findRestaurant(list1, list2);
+    cout<<answer<<endl;
+    return 0;
+}
