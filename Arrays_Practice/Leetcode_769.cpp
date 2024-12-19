2d92c4514d5c85423d165994abf8d245e7d6c5f3
imtheprince7 <princeks3577@gmail.com>
2024-12-19 22:14:52 +0530

    dec_19

---
+++ Arrays_Practice/Leetcode_769.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+         int ans = 0;
+    int mx = INT_MIN;
+
+    for (int i = 0; i < arr.size(); ++i) {
+      mx = max(mx, arr[i]);
+      if (mx == i)
+        ++ans;
+    }
+
+    return ans;
+    }
+};
+
+int main(){
+    Solution solution;
+    vector<int>  arr = {4,3,2,1,0};
+
+
+    cout<< solution.maxChunksToSorted(arr) <<endl;
+
+
+    return 0;
+}
